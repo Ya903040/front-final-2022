58e1428c32e85a1cc1f1de7f17b60a480e436a88
asiro <asirov560@gmail.com>
2022-12-19 21:21:54 +0600

    trying to do some style

--- src/components/screens/home/pages/About.jsx
+++ src/components/screens/home/pages/About.jsx
@@ -2,26 +2,37 @@ import React from "react";
 import {useNavigate} from "react-router-dom";
 import alatoo_logo from '../../../../assets/images/alatoo_logo.jpg';
 import './style/style.css';
+
 const About = () => {
     let navigate = useNavigate()
 
 
     return (
-        <div className=' w-full h-screen text-center'>
-            <p className="text-center justify-center text-4xl ">about this web-site</p>
-            <button onClick={() => {
-                navigate(('/front-final-2022/'))
-            }}
-                    className='text-orange-900 hover:text-purple-800 transition-colors easy-in-out duration-300 p-2 border-solid border-2 border-b-amber-400 rounded-xl text-2xl'>
-                change to home page
-            </button>
-            <p>hi everybody. My name is Suleiman.</p>
+        <div className='bg-common_flag w-full h-screen text-center'>
 
-            <div >
-                <img className='App-logo' src={alatoo_logo} alt='alatoo_logo'/>
+
+            <div className="block">
+                <p className="text-center justify-center text-4xl ">about this web-site</p>
+                <button onClick={() => {
+                    navigate(('/front-final-2022/'))
+                }}
+                        className='text-orange-900 hover:text-purple-800 transition-colors easy-in-out duration-300 p-2 border-solid border-2 border-b-amber-400 rounded-xl text-2xl'>
+                    change to home page
+                </button>
+                <p className='text-2xl p-20'>hi everybody. My name is Suleiman. I made this website using react js and
+                    tailwind.do not judge strictly. this is my first time using tailwind fully and I may have made a lot
+                    of
+                    mistakes. <br/>
+                    I apologize very much in advance.</p>
+
+                <div>
+                    <img className='App-logo' src={alatoo_logo} alt='alatoo_logo'/>
+                </div>
             </div>
 
+
         </div>
+
     );
 }
 
--- src/components/screens/home/pages/Country.jsx
+++ src/components/screens/home/pages/Country.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import usa_logo from "../../../../assets/images/logo_flag.png";
-
+import './style/style.css';
 const Country = () => {
     return(
-        <div className='w-full h-screen'>
+        <div id = "country_back" className='w-full h-screen text-center justify-center'>
 
 
-            <p className="text-center justify-center text-4xl" >this is about country</p>
-            <img className='App-logo ' src={usa_logo} alt="usa_logo"/>
+            <div className='block'>
+                <h1 className="text-4xl">Interesting facts about the USA.</h1>
+
+                <p className='pt-5 text-2xl'>1-)In Las Vegas, there is 1 slot machine for 8 citizens.</p>
+                <p className='pt-3 text-2xl'>2-)More than half of American adults drink at least one cup of coffee a day.</p>
+                <p className='pt-3 text-2xl'>3-)It was in the USA that the first mass-produced car was produced and the Internet was invented.</p>
+
+
+                <img className='App-logo pt-3' src={usa_logo} alt="usa_logo"/>
+            </div>
+
 
         </div>
     );
